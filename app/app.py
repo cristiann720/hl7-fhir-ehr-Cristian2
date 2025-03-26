a94a0af5b410b4d9b2fa4689c2547cfe248cb68b
cristiann720 <cristian.gonzalez-t@mail.escuelaing.edu.co>
2025-03-26 17:42:15 -0500

    x

--- app/app.py
+++ app/app.py
@@ -24,6 +24,17 @@ async def get_patient_by_id(patient_id: str):
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
 
+@app.get("/patient", response_model=dict)
+async def get_patient_by_iddentifier(system: str, value: str):
+    status,patient = GetPatientByIddentifier(system, value)
+    if status=='success':
+        return patient  # Return patient
+    elif status=='notFound':
+        raise HTTPException(status_code=404, detail="Patient not found")
+    else:
+        raise HTTPException(status_code=500, detail=f"Internal error. {status}")
+
+
 @app.post("/patient", response_model=dict)
 async def add_patient(request: Request):
     new_patient_dict = dict(await request.json())
--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb+srv://trillerascristian7:CristianIFMER720@cluster0.fdda0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    uri = "mongodb+srv://trillerascristian7:8412@cluster0.fdda0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
